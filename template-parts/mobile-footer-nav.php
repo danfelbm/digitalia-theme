c54c41120d2056433d3f845aaeddb08f7fdf2418
danfelbm <danfelbm@gmail.com>
2025-11-12 01:46:23 -0500

    Cambiar enlace mobile footer nav de /academia a /campus

--- template-parts/mobile-footer-nav.php
+++ template-parts/mobile-footer-nav.php
@@ -20,7 +20,7 @@
             </svg>
             <span class="text-xs mt-1">Transmedia</span>
         </a>
-        <a href="/academia" class="flex flex-col items-center justify-center text-white hover:text-gray-300">
+        <a href="/campus" class="flex flex-col items-center justify-center text-white hover:text-gray-300">
             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
                 <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
