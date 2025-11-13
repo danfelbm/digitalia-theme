cfb5c1e816c19d09201f358a87678f4fb1601693
danfelbm <danfelbm@gmail.com>
2025-11-12 19:02:28 -0500

    Fix: Eliminar show_in_rest duplicado en transmision ACF fields
    - Transmision tenía 'show_in_rest' => true en línea 18
    - Pero 'show_in_rest' => 0 en línea 149 lo sobrescribía
    - Eliminar declaración duplicada para exponer campos en REST API
    - Ahora /wp-json/wp/v2/transmision/{id} muestra todos los ACF fields
    - Verificado que TODOS los custom post types tienen show_in_rest correctamente

--- inc/acf_fields/transmision-acf-fields.php
+++ inc/acf_fields/transmision-acf-fields.php
@@ -146,7 +146,6 @@ if (!function_exists('digitalia_register_transmision_acf_fields')) {
                 'hide_on_screen' => '',
                 'active' => true,
                 'description' => '',
-                'show_in_rest' => 0,
             ));
         }
     }
